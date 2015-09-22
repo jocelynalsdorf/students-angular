2a2e0abb11b272b45d3f34a79d58d6f8cd8d263f
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-22 14:05:43 -0700

    initial file setup

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope){
+  $scope.students = [
+  {name: "Sam Schmidt"},
+  {name: "Ryan Samual"},
+  {name: "Jessica Martin"},
+  {name: "Brentwood Davis"}
+
+  ]
+});
