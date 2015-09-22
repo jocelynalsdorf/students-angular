8128c8c53e61a133aa8dd9fb1c2469189554609c
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-22 14:34:13 -0700

    adds form input and delelete of students functionality

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -1,9 +1,12 @@
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope){
-  $scope.students = [
-  {name: "Sam Schmidt"},
-  {name: "Ryan Samual"},
-  {name: "Jessica Martin"},
-  {name: "Brentwood Davis"}
-
-  ]
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({name: $scope.studentName}); //set value of name to the input with this directive
+    $scope.studentName = null; //set it to null so that the field in html is cleared
+  };
+  $scope.deleteStudent = function(student){
+    var index = $scope.students.indexOf(student);
+    $scope.students.splice(index,1);
+  };
+  
 });
