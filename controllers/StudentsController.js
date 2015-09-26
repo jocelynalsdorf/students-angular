fc1aa8172e87c2a34bb64f5313ed8c63657b7959
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-26 11:54:42 -0700

    add factory so that student and field trip permissoin controllers  can share information

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,12 +1,5 @@
-studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope){
-  $scope.students = [];
-  $scope.addStudent = function() {
-    $scope.students.push({name: $scope.studentName}); //set value of name to the input with this directive
-    $scope.studentName = null; //set it to null so that the field in html is cleared
-  };
-  $scope.deleteStudent = function(student){
-    var index = $scope.students.indexOf(student);
-    $scope.students.splice(index,1);
-  };
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory){
+  $scope.students = StudentsFactory.students;
+  $scope.StudentsFactory = StudentsFactory;
   
 });
